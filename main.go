b0c43b4e50ebf02c8caf6c850fc8615c270e537d
Kapil Agrawal <7047165+netops2devops@users.noreply.github.com>
2025-06-14 18:05:29 -0500

    middlewares and handlers

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"encoding/json"
+	"log"
 	"net/http"
 )
 
@@ -20,81 +20,20 @@ func Initializer() []Pets {
 	}
 }
 
-func RedirectGetPetHandler(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "/pets/", http.StatusMovedPermanently)
-}
-
-func RedirectPostPetHandler(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "/pets/", http.StatusMovedPermanently)
-}
-
-func ShowAllPetsHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	err := json.NewEncoder(w).Encode(p)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-}
-
-func GetPetHandler(w http.ResponseWriter, r *http.Request) {
-	name := r.PathValue("name")
-	for _, v := range p {
-		if v.Name == name {
-			w.Header().Set("Content-Type", "application/json")
-			json.NewEncoder(w).Encode(v)
-			return
-		}
-	}
-	w.WriteHeader(http.StatusNotFound)
-}
-
-func NewPetHandler(w http.ResponseWriter, r *http.Request) {
-	var newpet Pets
-	err := json.NewDecoder(r.Body).Decode(&newpet)
-	if err != nil {
-		http.Error(w, "Invalid JSON", http.StatusBadRequest)
-		return
-	}
-	defer r.Body.Close()
-
-	p = append(p, newpet)
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusCreated)
-	json.NewEncoder(w).Encode(p)
-}
-
-func DeletePetHandler(w http.ResponseWriter, r *http.Request) {
-	petName := r.PathValue("name")
-
-	for i, v := range p {
-		if v.Name == petName {
-			p = append(p[:i], p[i+1:]...)
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(http.StatusOK)
-			json.NewEncoder(w).Encode(map[string][]Pets{
-				"Pets": p,
-			})
-
-			return
-		}
-	}
-
-	http.Error(w, "Pet not found", http.StatusNotFound)
-}
-
 var p = Initializer()
 
 func main() {
-	http.HandleFunc("GET /pets", RedirectGetPetHandler)
-	http.HandleFunc("POST /pets", RedirectPostPetHandler)
+	mux := http.NewServeMux()
 
-	http.HandleFunc("GET /pets/", ShowAllPetsHandler)
-	http.HandleFunc("POST /pets/", NewPetHandler)
+	mux.HandleFunc("GET /pets/", ShowAllPetsHandler)
+	mux.Handle("POST /pets/", loggerMiddleware(authMiddleware(http.HandlerFunc(AddNewPetHandler))))
 
-	http.HandleFunc("GET /pets/{name}", GetPetHandler)
-	http.HandleFunc("DELETE /pets/{name}", DeletePetHandler)
+	mux.HandleFunc("GET /pets/{name}", GetPetHandler)
+	mux.Handle("DELETE /pets/{name}", loggerMiddleware(authMiddleware(http.HandlerFunc(DeletePetHandler))))
 
-	http.ListenAndServe(":8080", nil)
+	log.Print("server listening on port 8080")
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
---
+++ middleware.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+)
+
+func loggerMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := context.WithValue(r.Context(), "isLogged", true)
+		r = r.WithContext(ctx)
+		log.Print(time.Now(), r.Method, " ", r.RequestURI, r.WithContext(ctx))
+		next.ServeHTTP(w, r)
+	})
+}
+
+func authMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Header.Get("X-AUTH-TOKEN") != "secretlifeofpets" {
+			http.Error(w, "403 Forbidden", http.StatusForbidden)
+			return
+		}
+		ctx := context.WithValue(r.Context(), "isAuthenticated", true)
+		r = r.WithContext(ctx)
+		next.ServeHTTP(w, r)
+	})
+}
